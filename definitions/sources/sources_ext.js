c4ccaf6fd161cdf5873b1724653f43fababadd74
Rafael Carlos Dos Santos <rafael.carlos@autopass.com.br>
2026-04-22 11:47:51 +0000

    SUBINDO MONGO

--- definitions/sources/sources_ext.js
+++ definitions/sources/sources_ext.js
@@ -20,10 +20,11 @@ const tables = [
   "tb_ext_trs_tpd001_problems",
   "tb_ext_mrd_tpd001_locations",
   "tb_ext_mrd_tpd001_assetroomassignments",
-  "tb_ext_trs_mdb001_debit_transaction_m1_98",
+  "tb_ext_trs_mdb003_debit_transaction_m1_98",
   "tb_ext_mrd_tpd001_asseteqatmdetaillist",
   "tb_ext_mrd_tpd001_asseteqposqrcodedetaillist",
-  "tb_ext_trs_tmob001_sam_samtransactionfull"
+  "tb_ext_trs_tmob001_sam_samtransactionfull",
+  "tb_ext_trs_mdb003_debit_transaction_m1_238"
 
 ];
 
