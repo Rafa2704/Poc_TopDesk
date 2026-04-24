469d64be6eaea90c5147bf141b5234c237195fac
Rafael Carlos Dos Santos <rafael.carlos@autopass.com.br>
2026-04-24 14:49:25 +0000

    criando a itg

--- definitions/declarations/his_soures.js
+++ definitions/declarations/his_soures.js
@@ -1,8 +1,128 @@
+// definitions/declarations/his_sources.js
+
 declare({
-  database: 'prd-autopass-bi-001',   
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_card_usage',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_usage_data_trip',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_usage_data_service',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_usage_data_file',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_process_batch',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_clearing_payment',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_transport_provider',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_transport_provider_consortium',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_consortium',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_application',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
   schema: 'his',
   name: 'tb_his_mrd_top001_line_detail',
 });
 
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_line',
+});
 
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_line_direction',
+});
 
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_device_log_file',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_clearing',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_trs_top001_hot_list_single',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_card',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_card_user',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_user',
+});
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'his',
+  name: 'tb_his_mrd_top001_user_document',
+});
+// definitions/declarations/itg_sources.js
+
+declare({
+  database: 'prd-autopass-bi-001',
+  schema: 'itg',
+  name: 'tb_itg_trs_top001_card_emission',
+});
