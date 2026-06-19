9ecfa12337fa0a06d328ba108a05029f40f26d25
Rafael Carlos Dos Santos <rafael.carlos@autopass.com.br>
2026-06-19 16:43:10 +0000

    subindi dvc

--- definitions/sources/sources_ext.js
+++ definitions/sources/sources_ext.js
@@ -51,7 +51,8 @@ const tables = [
   "tb_ext_trs_tmb001_com_tranmt",
   "tb_ext_trs_tmb001_com_trandt",
   "tb_ext_mrd_tmb001_com_product",
-  "tb_ext_mrd_tmb001_medtransitproduct"
+  "tb_ext_mrd_tmb001_medtransitproduct",
+  "tb_ext_mrd_tmb001_dvc_device"
 
 ];
 
