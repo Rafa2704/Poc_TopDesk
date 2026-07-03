52bf84cb8308120f575a8fd026d16f0bdcad0071
Rafael Carlos Dos Santos <rafael.carlos@autopass.com.br>
2026-07-03 14:10:02 +0000

    criando as exts em prd

--- definitions/sources/sources_ext_dev.js
+++ definitions/sources/sources_ext_dev.js
@@ -3,9 +3,7 @@ const tables = [
 "tb_ext_mrd_shp001_depara_estacao",
 "tb_ext_mrd_shp001_depara_linha",
 "tb_ext_mrd_shp001_depara_consorcio",
-"tb_ext_mrd_shp001_depara_aplicacao",
-"tb_ext_mrd_shp001_depara_localidade_venda_atm",
-"tb_ext_mrd_shp001_depara_localidade_venda_bilheteria",
+"tb_ext_mrd_shp001_depara_aplicacao"
 
 ];
 
