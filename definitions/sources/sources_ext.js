1a1d568d88e6fd2da86ef2f680d9c9916313fa39
Rafael Carlos Dos Santos <rafael.carlos@autopass.com.br>
2026-08-03 14:23:31 +0000

    tb_ext_trs_tmb001_venda_qrcode

--- definitions/sources/sources_ext.js
+++ definitions/sources/sources_ext.js
@@ -90,7 +90,7 @@ const tables = [
   "tb_ext_mrd_tmb001_totransportoperator",
   "tb_ext_mrd_tmb001_tolinegroup",
   "tb_ext_trs_tmb001_app_qrcode",
-  "tb_ext_trs_tmob001_venda_qrcode"
+  "tb_ext_trs_tmb001_venda_qrcode"
 
 
 ];
